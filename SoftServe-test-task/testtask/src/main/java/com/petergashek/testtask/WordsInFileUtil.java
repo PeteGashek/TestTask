b68777bee3cda7788a67e55ed4f7b684338a498a
PeterGashek <PeterGashek@192.168.0.102>
2015-05-13 19:26:32 +0300

    added proper comments

--- SoftServe-test-task/testtask/src/main/java/com/petergashek/testtask/WordsInFileUtil.java
+++ SoftServe-test-task/testtask/src/main/java/com/petergashek/testtask/WordsInFileUtil.java
@@ -4,104 +4,109 @@ import java.io.*;
 import java.util.*;
 import java.util.Map.Entry;
 
-public final class WordsInFileUtil {
-	
-	private WordsInFileUtil (){}
+public final class WordsInFileUtil
+{
 
-	public static Map<String, Integer> getWords(String nameOfFile) // Method to get
-															// words from file
-	{
+   private WordsInFileUtil()
+   {
+   }
 
-		FileInputStream fiStream = null;
-		DataInputStream diStream = null;
-		BufferedReader bReader = null;
-		String targetLine = null;
+   public static Map<String, Integer> getWords( String nameOfFile ) // Method to get words from file
 
-		Map<String, Integer> wordMap = new HashMap<String, Integer>();
+   {
 
-		try {
-			fiStream = new FileInputStream(nameOfFile);
-			diStream = new DataInputStream(fiStream);
-			bReader = new BufferedReader(new InputStreamReader(diStream));
+      FileInputStream fiStream = null;
+      DataInputStream diStream = null;
+      BufferedReader bReader = null;
+      String targetLine = null;
 
-			while ((targetLine = bReader.readLine()) != null) {
-				StringTokenizer sTokenizer = new StringTokenizer(targetLine,
-						" ");
+      Map<String, Integer> wordMap = new HashMap<String, Integer>();
 
-				while (sTokenizer.hasMoreTokens()) {
-					String buffer = sTokenizer.nextToken().toLowerCase(); // to
-																			// prevent
-																			// case
-																			// sensitive
-																			// collisions
-					if (wordMap.containsKey(buffer)) {
-						wordMap.put(buffer, wordMap.get(buffer) + 1);
-					} else {
-						wordMap.put(buffer, 1);
-					}
-				}
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("File Not found.");
-		} catch (IOException e) {
-			System.out.println("Invalid file.");
-		} finally {
+      try
+      {
+         fiStream = new FileInputStream(nameOfFile);
+         diStream = new DataInputStream(fiStream);
+         bReader = new BufferedReader(new InputStreamReader(diStream));
 
-			try {
-				if (bReader != null)
-					bReader.close();
-			} catch (Exception ex) {
-			}
-		}
-		return wordMap;
-	}
+         while ((targetLine = bReader.readLine()) != null)
+         {
+            StringTokenizer sTokenizer = new StringTokenizer(targetLine, " ");
 
-	public static List<Entry<String, Integer>> getMaxOccurence(
-			Map<String, Integer> wordMap) // Method that encapsulates
-											// comparative logic
-	{
+            while (sTokenizer.hasMoreTokens())
+            {
+               String buffer = sTokenizer.nextToken().toLowerCase(); // to prevent case sensitive collisions
 
-		Set<Entry<String, Integer>> wordSet = wordMap.entrySet();
-		List<Entry<String, Integer>> resulsList = new ArrayList<Entry<String, Integer>>(
-				wordSet);
-		Collections.sort(resulsList,
-				new Comparator<Map.Entry<String, Integer>>() // here we
-																// redefining
-																// comparator
-																// for our
-																// purposes
-				{
-					public int compare(Map.Entry<String, Integer> occurence,
-							Map.Entry<String, Integer> nextOccurence) {
-						return (nextOccurence.getValue()).compareTo(occurence
-								.getValue());// if switch places of occurrences
-												// we get acceding or decceding
-												// output
-					}
-				});
-		return resulsList;
-	}
+               if (wordMap.containsKey(buffer))
+               {
+                  wordMap.put(buffer, wordMap.get(buffer) + 1);
+               }
+               else
+               {
+                  wordMap.put(buffer, 1);
+               }
+            }
+         }
+      }
+      catch (FileNotFoundException e)
+      {
+         System.out.println("File Not found.");
+      }
+      catch (IOException e)
+      {
+         System.out.println("Invalid file.");
+      }
+      finally
+      {
 
-	public static List<String> topFiveOutput(String path) {
+         try
+         {
+            if (bReader != null)
+               bReader.close();
+         }
+         catch (Exception ex)
+         {
+         }
+      }
+      return wordMap;
+   }
 
-		Map<String, Integer> wordMap = getWords(path);
+   public static List<Entry<String, Integer>> getMaxOccurence( Map<String, Integer> wordMap ) // Method that encapsulates comparative logic
+   {
 
-		List<Entry<String, Integer>> resultList = getMaxOccurence(wordMap);
-		List<String> returnedResult = new ArrayList<String>(5);
-		int counter = 0;
-		for (Map.Entry<String, Integer> entry : resultList) {
+      Set<Entry<String, Integer>> wordSet = wordMap.entrySet();
+      List<Entry<String, Integer>> resulsList = new ArrayList<Entry<String, Integer>>(wordSet);
+      Collections.sort(resulsList, new Comparator<Map.Entry<String, Integer>>() // here we redefining comparator for our purposes
 
-			returnedResult.add(entry.getKey() + " occured " + entry.getValue()
-					+ " time(s)");
-			counter++;
-			if (counter == 5) { // not very common and loved but understandable
-								// solution to get top five limit
-				break;
-			}
-		}
+      {
+         public int compare( Map.Entry<String, Integer> occurence, Map.Entry<String, Integer> nextOccurence )
+         {
+            return (nextOccurence.getValue()).compareTo(occurence.getValue());// if we switch places of occurrences we get acceding or decceding output
+         }
+      });
+      return resulsList;
+   }
 
-		return returnedResult;
+   public static List<String> topFiveOutput( String path ) // Method for endUser to run
+   {
 
-	}
+      Map<String, Integer> wordMap = getWords(path);
+
+      List<Entry<String, Integer>> resultList = getMaxOccurence(wordMap);
+      List<String> returnedResult = new ArrayList<String>(5);
+      int counter = 0;
+      for (Map.Entry<String, Integer> entry : resultList)
+      {
+
+         returnedResult.add(entry.getKey() + " occured " + entry.getValue() + " time(s)");
+         counter++;
+         if (counter == 5) // not very common and loved but understandable solution to get top five limit
+         { 
+            break;
+         }
+      }
+
+      return returnedResult;
+
+   }
 
 }
