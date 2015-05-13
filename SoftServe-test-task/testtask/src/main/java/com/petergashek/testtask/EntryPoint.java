4d2c1bc2bcc514db154034b09a2fd81651a7ccbd
PeterGashek <PeterGashek@192.168.0.102>
2015-05-13 19:22:20 +0300

    added code format

--- SoftServe-test-task/testtask/src/main/java/com/petergashek/testtask/EntryPoint.java
+++ SoftServe-test-task/testtask/src/main/java/com/petergashek/testtask/EntryPoint.java
@@ -2,15 +2,17 @@ package com.petergashek.testtask;
 
 import java.util.List;
 
-public class EntryPoint {
+public class EntryPoint
+{
 
-	public static void main(String[] args) {
+   public static void main( String[] args )
+   {
 
-		List<String> topFiveOutput = WordsInFileUtil
-				.topFiveOutput("/Users/ghost/Documents/words.txt");
-		for (String string : topFiveOutput) {
-			System.out.println(string);
+      List<String> topFiveOutput = WordsInFileUtil.topFiveOutput("/Users/ghost/Documents/words.txt");
+      for (String string : topFiveOutput)
+      {
+         System.out.println(string);
 
-		}
-	}
+      }
+   }
 }
